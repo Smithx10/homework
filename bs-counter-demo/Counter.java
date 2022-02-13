648cf22398d0365c2d6a6ed756e9ccca14d00d5c
Bruce Smith <b.smith@us.imshealth.com>
2022-02-13 18:56:50 -0500

    simpler

---
+++ bs-counter-demo/Counter.java
@@ -0,0 +1,9 @@
+public class Counter {
+    private int num;
+    public Counter(){
+        num = 0;
+    }
+    public void printNum(){
+        System.out.println("Current Value: " + num);  
+    }
+}
