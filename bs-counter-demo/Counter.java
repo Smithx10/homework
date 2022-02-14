fc2b2f86a37bcbd491701547e65f242ee5f86b43
Bruce Smith <b.smith@us.imshealth.com>
2022-02-13 20:31:05 -0500

    We mutat1n brah

--- bs-counter-demo/Counter.java
+++ bs-counter-demo/Counter.java
@@ -1,9 +1,73 @@
 public class Counter {
-    private int num;
+    // Privates are attr's on the class / type that are used only in the clas
+    // count is where we are at in the count
+    private int count;
+    // name is what we name our Counter
+    //private String name;
+
+    // Constructor
     public Counter(){
-        num = 0;
+        count = 0;
     }
-    public void printNum(){
-        System.out.println("Current Value: " + num);  
+
+    // Bump the Count 1
+    public void AddOne(){
+      count = count + 1;
     }
+
+    // Bump the Count by param
+    public void AddByAddorator(int addorator){
+      count = count + addorator;
+    }
+
+    // Sub the Count by param
+    public void SubBySubtrominator(int subtrominator){
+      if (count - subtrominator < 0) {
+        // Sad Place
+        System.out.println("Can't go Lower than 0 bro.");
+        // Probably should return an error
+        return;
+      }
+      count = count - subtrominator;
+    }
+
+    // Bump the Count by param
+    public void MutateCount(String direction, int number){
+      // toLower the direction so our switch can match
+      switch (direction) {
+        case "up":
+          System.out.println("Brah..... why....");
+          AddByAddorator(number);
+          return;
+          // Logic for Addition
+        case "down":
+          SubBySubtrominator(number);
+          return;
+          // Logic for Subtraction
+        default: 
+        System.out.println("Brah..... gotta be up or down");
+      }
+    }
+
+    // Sub the Count 1
+    public void SubOne(){
+      if (count - 1 < 0) {
+        // Sad Place
+        System.out.println("Can't go Lower than 0 bro.");
+        // Probably should return an error
+        return;
+      }
+
+      // Happy Path
+      count = count - 1;
+    }
+    
+    public void PrintCount(){
+        System.out.println("Current Value: " + count);  
+    }
+
+    public String toString(){
+      return Integer.toString(count);
+    }
+
 }
--- bs-counter-demo/Main.java
+++ bs-counter-demo/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-      Counter CounterOne = new Counter();
-      CounterOne.printNum();
+      // We construct a Counter by calling new on our public Constructor "Counter()"
+      Counter c1 = new Counter();
+
+
+      c1.AddOne();
+      c1.MutateCount("up", 1337);
+      c1.PrintCount();
+
+      String currentCount = c1.toString();
+        System.out.println("rawr: "+ currentCount);
+
+
+
+      // Call PrintCount to see where we are.
+      //CounterOne.PrintCount();
+      //CounterOne.SubOne();
+      //CounterOne.PrintCount();
+      //CounterOne.AddOne();
+      //CounterOne.AddByAddorator(42);
+      //CounterOne.PrintCount();
     }
 }
