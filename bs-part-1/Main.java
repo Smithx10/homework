d82f920e3b56f879da0a769c8cbba2009acc94ac
Bruce Smith <b.smith@us.imshealth.com>
2022-02-15 13:06:08 -0500

    minimal example

--- bs-part-1/Main.java
+++ bs-part-1/Main.java
@@ -2,6 +2,10 @@ public class Main {
     // the main() function is where our program starts.
     public static void main(String[] args) {
 
+      // Construct a counter and store it in the variable "c1"
       Counter c1 = new Counter();
+      
+      // Call the PrintNumb function / method from the counter class of the instance c1.
+      c1.PrintNum();
     }
 }
