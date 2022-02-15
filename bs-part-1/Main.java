b89624cd8b0133c5bee4739f56789e8df0eba129
Bruce Smith <b.smith@us.imshealth.com>
2022-02-15 13:06:45 -0500

    minimal example

--- bs-part-1/Main.java
+++ bs-part-1/Main.java
@@ -5,7 +5,7 @@ public class Main {
       // Construct a counter and store it in the variable "c1"
       Counter c1 = new Counter();
       
-      // Call the PrintNumb function / method from the counter class of the instance c1.
-      c1.PrintNum();
+      // Call the PrintCount function / method from the counter class of the instance c1.
+      c1.PrintCount();
     }
 }
